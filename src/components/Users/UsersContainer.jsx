071f36ec658315f36bdfdb5bf62b02a47950d29c
ZemtsovAY <3emtsovAY@gmail.com>
2020-08-21 13:13:59 +0400

    add img users

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,20 +1,34 @@
 import {connect} from "react-redux";
 import Users from "./Users";
-import {setFollowedAC} from "../../redux/users-reducer";
+import {followAC, unfollowAC, setUsersAC, setCurrentPageAC} from "../../redux/users-reducer";
 import React from "react";
 import * as axios from 'axios'
 
 class UsersAPIComponent extends React.Component {
     componentDidMount() {
-        // axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        //     .then((response) => {
-        //         console.log(response);
-        //     })
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+            })
     }
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
     render(){
         return (<Users
-            followed={this.props.followed}
-            setFollowed = {this.props.setFollowed}
+            users = {this.props.users}
+            follow = {this.props.follow}
+            unfollow = {this.props.unfollow}
+            pageSize = {this.props.pageSize}
+            totalUsersCount={this.props.totalUsersCount}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            //setCurrentPage={this.props.setCurrentPage}
 
         />)
     }
@@ -23,13 +37,19 @@ class UsersAPIComponent extends React.Component {
 let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
-        followed: state.usersPage.followed
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
 let mapDispatchToProps = (dispatch) => {
     return {
-        setFollowed: (followed) => dispatch(setFollowedAC(followed))
+        setUsers: (users) => dispatch(setUsersAC(users)),
+        follow: (userId) => dispatch(followAC(userId)),
+        unfollow: (userId) => dispatch(unfollowAC(userId)),
+        setCurrentPage: (currentPage) => dispatch(setCurrentPageAC(currentPage))
+
     }
 }
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,15 @@
 const SET_USERS = 'SET_USERS'
-const SET_FOLLOWED = 'SET_FOLLOWED'
+const FOLLOW = 'FOLLOW'
+const UNFOLLOW = 'UNFOLLOW'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+
+
 
 let initialState = {
     users: [],
-    followed: false
+    pageSize: 5,
+    totalUsersCount: 41,
+    currentPage: 1
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -11,12 +17,34 @@ const usersReducer = (state = initialState, action) => {
         case SET_USERS:
             return {
                 ...state,
-                users: [...state.users, ...action.users]
+                users: [...action.users]
             }
-        case SET_FOLLOWED:
+        case FOLLOW:
             return {
                 ...state,
-                followed: action.followed
+                users: state.users.map(user => {
+                    if (user.id === action.userId) {
+                        return {...user, followed: true}
+                    }
+                    return user
+                })
+
+
+            }
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(user => {
+                    if (user.id === action.userId) {
+                        return {...user, followed: false}
+                    }
+                    return user
+                })
+            }
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.currentPage
             }
         default:
             return state
@@ -29,9 +57,19 @@ export const setUsersAC = (users) => ({
     users: users
 })
 
-export const setFollowedAC = (followed) => ({
-    type: SET_FOLLOWED,
-    followed: followed
+export const setCurrentPageAC = (currentPage) => ({
+    type: SET_CURRENT_PAGE,
+    currentPage: currentPage
+})
+
+export const followAC = (userId) => ({
+    type: FOLLOW,
+    userId: userId
+})
+
+export const unfollowAC = (userId) => ({
+    type: UNFOLLOW,
+    userId: userId
 })
 
 
